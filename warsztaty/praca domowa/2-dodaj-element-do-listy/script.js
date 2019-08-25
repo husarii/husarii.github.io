2548a7a8dea0e0619b9338663dbccf4d3f0e1a3e
Sara Kordonowska <sara.kordonowska@hotmail.com>
2019-08-25 16:32:04 +0200

    update tygodniowy

--- warsztaty/praca domowa/2-dodaj-element-do-listy/script.js
+++ warsztaty/praca domowa/2-dodaj-element-do-listy/script.js
@@ -1,28 +1,33 @@
 let items = document.getElementById("items");
 console.log (items)
 
-// document.getElementsByTagName("button").addEventListener("click", addItem ); 
 let przycisk = document.getElementsByTagName ("button")
+przycisk[0].addEventListener("click", addItem ); 
+
 
 console.log (przycisk)
 
 function addItem(){
-  console.log ("click");
+  
   let liElem = document.getElementsByClassName("item");
+  console.log(liElem);
 
-    for (i = 0; i < liElem.length; i++){
+    //for (i = 0; i < liElem.length; i++){
 
       let newItem = document.createElement('li');
+      //console.log(newItem)
       
       let classAtr = document.createAttribute("class");
       classAtr.value= "item";
       newItem.setAttributeNode( classAtr );
     
-      newItem.innerHTML = 'Item' + [liElem.length+1];
+      newItem.innerHTML = 'Item ' + [liElem.length+1];
+      //console.log (newItem)
+
       items.appendChild (newItem);
-    
+        
   }
 
-}
 
-przycisk.onclick = addItem(); 
+
+// przycisk.onclick = addItem(); 
